262aab43f2d64cba7aded22172275dce7ff53aab
VedantPatle0305 <vedantpatle594@gmail.com>
2025-08-29 10:40:45 +0530

    Cart Item Retained

--- Fake Store/ViewModel/CartViewModel.swift
+++ Fake Store/ViewModel/CartViewModel.swift
@@ -9,7 +9,17 @@ import Foundation
 
 @MainActor
 class CartViewModel: ObservableObject {
-    @Published var cartItems: [Product] = []
+    @Published var cartItems: [Product] = []{
+        didSet {
+            saveCart()
+        }
+    }
+    
+    private let cartKey = "savedCart"
+    
+    init() {
+        loadCart()
+    }
     
     func toggleCart(item: Product) {
         if cartItems.contains(item) {
@@ -26,4 +36,18 @@ class CartViewModel: ObservableObject {
     func clearCart() {
         cartItems.removeAll()
     }
+    
+    func saveCart() {
+        if let encodedData = try? JSONEncoder().encode(cartItems) {
+            UserDefaults.standard.set(encodedData, forKey: cartKey)
+        }
+    }
+    
+    func loadCart() {
+        if let data = UserDefaults.standard.data(forKey: cartKey),
+           let decoded = try? JSONDecoder().decode([Product].self, from: data) {
+            cartItems = decoded
+        }
+    }
+    
 }
